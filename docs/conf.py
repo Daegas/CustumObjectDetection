03cc73711f4ba42436776df5b0dd450c3b00b8f8
Daegas <darae.gs@gmail.com>
2020-10-08 12:33:42 -0500

    Docs press-them try

--- docs/conf.py
+++ docs/conf.py
@@ -54,12 +54,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_pdj_theme'
-import sphinx_pdj_theme
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
-html_theme_options = {
-    'style': 'darker'
-}
+html_theme = 'press'
+# import sphinx_pdj_theme
+# html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+# html_theme_options = {
+#     'style': 'darker'
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
